19ae2ea2bf8a71f5a9ce975de87309d9dff85444
schlagmt <mtschlager@gmail.com>
2022-05-05 17:34:53 -0400

    WIP

--- Diversify/ClientApp/src/components/Home.js
+++ Diversify/ClientApp/src/components/Home.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    render() {
+
+        if (!sessionStorage.getItem("Diversify-Bearer-Token")) {
+            var request = require('request'); // "Request" library
+
+            var client_id = '752407c80ece4143822dba70f3b8747e'; // Your client id
+            var client_secret = 'dcc0fdd5e1a846d29ad42c36bfb2ef04'; // Your secret
+
+            // your application requests authorization
+            var authOptions = {
+                url: 'https://accounts.spotify.com/api/token',
+                headers: {
+                    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+                },
+                form: {
+                    grant_type: 'client_credentials'
+                },
+                json: true
+            };
+
+            request.post(authOptions, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    sessionStorage.setItem("Diversify-Bearer-Token")
+                }
+            });
+        }
+
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <p>Welcome to your new single-page application, built with:</p>
+                <ul>
+                    <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
+                    <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
+                    <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+                </ul>
+                <p>To help you get started, we have also set up:</p>
+                <ul>
+                    <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
+                    <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
+                    <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
+                </ul>
+                <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+            </div>
+        );
+    }
 }
---
+++ Diversify/Controllers/UserTopsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diversify.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserTopsController : ControllerBase
+    {
+        private readonly ILogger<UserTopsController> _logger;
+
+        public UserTopsController(ILogger<UserTopsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
